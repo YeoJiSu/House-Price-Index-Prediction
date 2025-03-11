8d18b0b7f3f8990f30721ea0c1a5592c5370aeba
YeoJiSu <duwltn1301@naver.com>
2025-03-11 19:55:22 +0900

    Configuration

---
+++ model/config.py
@@ -0,0 +1,45 @@
+import os
+
+"""
+Setting for South Korea
+"""
+# File path settings
+FEATURE_PATH = os.path.join("..", "data", "KR_explainable_variables.csv")
+TARGET_PATH  = os.path.join("..", "data", "KR_target_variables.csv")
+
+# Date settings
+TRAIN_DATE     = '2006-01-15'
+TEST_DATE      = '2021-01-15'
+REFERENCE_DATE = '2017-11-15'   # Base date
+
+# Kalman Filter Covariance Setting
+KALMAN_OBSERVATION_COV = 10      # Measurement noise
+KALMAN_TRANSITION_COV   = 0.01   # State transition noise
+
+# VAR model parameters
+MAXLAG_FACTOR    = 23
+SELECTED_LAG_FACTOR = 3
+FORECAST_HORIZON  = 3  # Forecast steps (e.g., 6-month prediction)
+WINDOW_SIZE = 3
+
+# Deep learning model settings
+MODEL_NAME     = "PatchLatentMLP"
+FORECAST_SIZE  = FORECAST_HORIZON 
+VERSION        = f"{FORECAST_SIZE}{MODEL_NAME}_{KALMAN_OBSERVATION_COV}_{KALMAN_TRANSITION_COV}"
+DIR_PATH       = "/Users/yeojisu/Documents/HPI-Save/example/"
+
+# Common text (if needed)
+COMMON_TXT = f"maxlag={MAXLAG_FACTOR}selected_lag={SELECTED_LAG_FACTOR}kalman={KALMAN_OBSERVATION_COV}&{KALMAN_TRANSITION_COV}"
+
+"""
+Setting for United States
+
+# File path settings
+FEATURE_PATH = os.path.join("..", "data", "US_explainable_variables.csv")
+TARGET_PATH  = os.path.join("..", "data", "US_target_variables.csv")
+
+# Date settings
+TRAIN_DATE     = '2004-01-01'
+TEST_DATE      = '2020-01-01'
+REFERENCE_DATE = '2004-01-01'   # Base date
+"""
